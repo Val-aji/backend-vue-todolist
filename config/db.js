21809e37cd6fd4f2d26424dad7c91f975f882de7
Nouval <snouval4@gmail.com>
2023-03-22 21:39:18 +0700

    tes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 import { Sequelize } from "sequelize";
 import mysql2 from "mysql2"
 
-const DB_NAME = "nouvalaj_warungonline"
-const DB_HOST = "localhost"
-const DB_PASSWORD = "6Fs+xDpD@MIb"
-const DB_PORT = 3306
-const DB_USER = "root"
+// const DB_NAME = "nouvalaj_warungonline"
+// const DB_HOST = "localhost"
+// const DB_PASSWORD = "6Fs+xDpD@MIb"
+// const DB_PORT = 3306
+// const DB_USER = "root"
+
+const DB_HOST = process.env.DB_HOST || "megatron.idserverhost.com"
+const DB_PORT = process.env.DB_PORT || 3306
+const DB_NAME = process.env.DB_NAME || "nouvalaj_todolist"
+const DB_USER = process.env.DB_USER || "nouvalaj_admin"
+const DB_PASSWORD = process.env.DB_PASSWORD || "Y0i@I2cVz(-G"
 
 const db = new Sequelize({
   username: DB_USER,
