5672321ef25290307d6dc15100aae3eb123bece7
Nouval <snouval4@gmail.com>
2023-03-22 13:10:08 +0700

    konfigurasi database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const DB_NAME = "railway"
-const DB_HOST = "containers-us-west-20.railway.app"
-const DB_PASSWORD = "x4tcNPTqhlFBytTuCDks"
-const DB_PORT = 5474
+const DB_NAME = "nouvalaj_warungonline"
+const DB_HOST = "localhost"
+const DB_PASSWORD = "6Fs+xDpD@MIb"
+const DB_PORT = 3306
 const DB_USER = "root"
 
 const db = new Sequelize({
